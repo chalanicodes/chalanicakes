89f04357497a91e6a7acb84197e46bdae3048cb8
Mill Daulagala <milldaulagala@192-168-1-101.tpgi.com.au>
2019-04-30 13:39:54 +1000

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+const Navigation = (props) => (
+  <div id="myNav" className="overlay">
+    <a className="closebtn" onClick={() => props.closeNav()}>&times;</a>
+    <div className="overlay-content">
+      <Link to="/" onClick={() => props.closeNav()}>Home</Link>
+      <Link to="/birthday" onClick={() => props.closeNav()}>Birthday Cakes</Link>
+      <a href="">Black Forest Cake</a>
+      <a href="">Butter Cake</a>
+      <a href="">Sponge Cake</a>
+    </div>
+  </div>
+);
+
+export default Navigation;
